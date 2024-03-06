647f05bba17fd889dfa80f6a91dfad0d4db093e2
Dorion-M <damcbv@umsystem.edu>
2024-03-06 12:02:39 -0600

    Added comments to the code

--- HomeWork/4320/FizzBuzz_4320.py
+++ HomeWork/4320/FizzBuzz_4320.py
@@ -1,28 +1,21 @@
+#Print numbers from 1 to 100
+#For multiples of 3, replace the number with 'Fizz'
+#For multiples of 5, replace the number with 'Buzz'
+#For multiples of both, replace the number with 'FizzBuzz'
+
 def fizzbuzz_clean():
-
-    #Iterates from 1 - 100
+    
     for num in range(1, 101):
-
-        # Check if the number is divisible by 3 (Fizz)
         fizz = (num % 3 == 0)
-
-        # Check if the number is divisible by 5 (Buzz)
         buzz = (num % 5 == 0)
         
-        # Check for FizzBuzz condition (divisible by both 3 and 5)
         if fizz and buzz:
             print("FizzBuzz")
-
-        # Check for Fizz condition (divisible by 3 only)    
         elif fizz:
             print("Fizz")
-
-        # Check for Fizz condition (divisible by 5 only)    
         elif buzz:
             print("Buzz")
         else:
             print(num)
 
-
-# Calls the function to execute FizzBuzz
 fizzbuzz_clean() 
